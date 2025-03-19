88b99f0a62e98e699db4a2dc55b54ff9dce42123
installer <installer@gmail.com>
2025-03-19 15:16:51 +0000

    remove dupe nix settings

--- hosts/cyh-nixos/configuration.nix
+++ hosts/cyh-nixos/configuration.nix
@@ -411,11 +411,4 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
-  
-  # Other NixOS Settings
-  nix.settings.experimental-features = [
-  	"nix-command"
-  	"flakes"
-  ];
-
 }
